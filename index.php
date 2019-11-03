dd986052bec4152f667b57b6964cc41d1befd219
michsko544 <michsko544@student.polsl.pl>
2019-11-03 03:43:08 +0100

    index

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Recruitment System</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <div id="sign-in">
+        <div class="nav-bar">
+            
+        </div>
+        <div class="input-wrapper">
+            <form>
+                <label for="login">Login</label>
+                <input type="text" placeholder="Username">
+                <label for="password">Password</label>
+                <input type="password" placeholder="**********">
+                <input type="submit" value="Sign in">
+            </form>
+        </div>
+    </div>
+    <a href="#"><div class="btn-sign-in">Sign in</div></a>
+    <div id="index-container">
+        <div class="center-wrapper">
+            <div class="logo-wrapper">
+                <div class="logo">myCompany</div>
+                <div class="logo-text"><span style="color:#36C3D9;">Apply</span> to us easily</div>
+                <div class="index-btn-container">
+                    <a href="#"><div class="btn-dark">Join us!</div></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
+</body>
+</html>
