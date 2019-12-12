e67424cba41fc60b3c1728bbe4cc1be2b6035a76
kchodkiewicz <you@example.com>
2019-12-13 00:08:39 +0100

    zjebalem

--- php/Profile.php
+++ php/Profile.php
@@ -1,15 +1,7 @@
 <?php
-
-
+require_once "connect.php";
 class Profile
 {
-    /*  TODO display multiple experience values
-                                set varGet to $count_tx - 1
-                                do magic
-                                make it work
-                                create loop with echo with whole form and fill it
-                                with data according to $i value
-                            */
     private $experience_form_p1 = " <div class=\"list-row\">
             <div class=\"title-element\">Experience</div>
             <div class=\"btn-element\">
@@ -135,7 +127,7 @@ class Profile
     private $skills_form_p2 = "' required>
                     <div class=\"underline\"></div>
                 <div class=\"degree\">
-                    <input type=\"number\" name=\"language-level-0\" min=1 max=5 placeholder=1 value=\'";
+                    <input type=\"number\" name=\"language-level-0\" min=1 max=5 placeholder=1 value='";
 
     private $skills_form_p3 = "'>
                         <div class=\"limit\">/5</div>
@@ -158,7 +150,7 @@ class Profile
     private $skills_form_p4 = "' required>
                     <div class=\"underline\"></div>
                     <div class=\"degree\">
-                        <input type=\"number\" name=\"skill-level-0\" min=1 max=5 placeholder=1 value=\'";
+                        <input type=\"number\" name=\"skill-level-0\" min=1 max=5 placeholder=1 value='";
 
     private $skills_form_p5 = "'>
                         <div class=\"limit\">/5</div>
@@ -232,7 +224,106 @@ class Profile
     private $skills_form = "";
     private $additional_form = "";
 
+    private $tmp_x = "";
+    private $tmp_e = "";
+    private $tmp_l = "";
+    private $tmp_s = "";
+    private $tmp_a = "";
+
+    private $count_tx = 0;
+    private $count_te = 0;
+    private $count_tl = 0;
+    private $count_ts = 0;
+    private $count_ta = 0;
+
     // methods
+    function fetchData($host, $db_user, $db_pass, $db_name)
+    {
+        mysqli_report(MYSQLI_REPORT_STRICT);
+        try
+        {
+            $connection = new mysqli($host, $db_user, $db_pass, $db_name);
+            if ($connection->connect_errno != 0)
+            {
+                throw new Exception(mysqli_connect_errno());
+            } else
+            {
+                // table 2
+                $table_experience = $connection->query("SELECT e.job, e.employer, e.start_job, e.end_job, c.locality As job_city, e.description As job_description from users u join applicants a on u.id_user=a.id_user join cities c on a.id_city=c.id_city join experiences e on a.id_applicants = e.id_applicants where u.id_user='{$_SESSION['id_user']}'");
+                if (!$table_experience)
+                {
+                    throw new Exception($connection->error);
+                }
+                $this->count_tx = $table_experience->num_rows;
+                $assoc_tx = $table_experience->fetch_assoc();
+
+                // table 3
+                $table_education = $connection->query("SELECT s.name_school, s.specialization, s.start_learning, s.end_learning, c.locality As school_city, s.description As school_description from users u join applicants a on u.id_user=a.id_user join cities c on a.id_city=c.id_city join schools s on a.id_applicants=s.id_applicants where u.id_user='{$_SESSION['id_user']}'");
+                if (!$table_education)
+                {
+                    throw new Exception($connection->error);
+                }
+                $this->count_te = $table_education->num_rows;
+                $assoc_te = $table_education->fetch_assoc();
+
+                // table 4.1
+                $table_lang = $connection->query("SELECT la.language FROM users u join applicants a on u.id_user=a.id_user join knowledge k on a.id_applicants=k.id_applicants join levels le on k.id_level=le.id_level JOIN languages la on k.id_language=la.id_language where u.id_user = '{$_SESSION['id_user']}'");
+                $table_lang_level = $connection->query("SELECT le.id_level FROM users u join applicants a on u.id_user=a.id_user join knowledge k on a.id_applicants=k.id_applicants join levels le on k.id_level=le.id_level JOIN languages la on k.id_language=la.id_language where u.id_user = '{$_SESSION['id_user']}'");
+                if (!$table_lang || !$table_lang_level)
+                {
+                    throw new Exception($connection->error);
+                }
+                $this->count_tl = $table_lang->num_rows;
+                while ( $assoc_tl = $table_lang->fetch_assoc())
+                {
+                    while ((int)$assoc_tll = $table_lang_level->fetch_assoc())
+                        foreach ($assoc_tl as $key=>$value)
+                        {
+                            foreach ($assoc_tll as $keyL=>$valueL)
+                            {
+                                $this->tmp_l = $this->tmp_l . $this->skills_form_mux1 . $value . $this->skills_form_p2 . $valueL . $this->skills_form_p3;
+                            }
+                        }
+                }
+
+                // table 4.2
+                $table_skills = $connection->query("SELECT s.sience, le.id_level FROM users u join applicants a on u.id_user=a.id_user join holders k on a.id_applicants=k.id_applicants join levels le on k.id_level=le.id_level join holders h on le.id_level=h.id_level join skills s on s.id_skill=h.id_skill where u.id_user = '{$_SESSION['id_user']}'");
+                $table_skills_level = $connection->query("SELECT le.id_level FROM users u join applicants a on u.id_user=a.id_user join holders k on a.id_applicants=k.id_applicants join levels le on k.id_level=le.id_level join holders h on le.id_level=h.id_level join skills s on s.id_skill=h.id_skill where u.id_user = '{$_SESSION['id_user']}'");
+                if (!$table_skills || !$table_skills_level)
+                {
+                    throw new Exception($connection->error);
+                }
+                $this->count_ts = $table_skills->num_rows;
+                while ($assoc_ts = $table_skills->fetch_assoc())
+                {
+                    while ((int)$assoc_tsl = $table_skills_level->fetch_assoc())
+                    foreach ($assoc_ts as $key=>$value)
+                    {
+                        foreach ($assoc_tsl as $keyL=>$valueL)
+                        {
+                            $this->tmp_s = $this->tmp_s . $this->skills_form_mux3 . $value . $this->skills_form_p4 . $valueL . $this->skills_form_p5;
+                        }
+                    }
+                }
+
+                // table 5
+                $table_additional = $connection->query("SELECT cv.description As cv_description, cl.description As cl_description, certifications.descriptions As cert_descriptions, t.training, t.description As course_description from users u join applicants a on u.id_user=a.id_user join cv on a.id_applicants=cv.id_applicants join certifications on a.id_applicants=certifications.id_applicants join training t on a.id_applicants=t.id_applicants join applications ap on a.id_applicants=ap.id_applicants join cl on ap.id_application=cl.id_application where u.id_user='{$_SESSION['id_user']}'");
+                if (!$table_additional)
+                {
+                    throw new Exception($connection->error);
+                }
+                $this->count_ta = $table_additional->num_rows;
+                $assoc_ta = $table_additional->fetch_assoc();
+            }
+
+            $connection->close();
+        }
+        catch (Exception $e)
+        {
+            echo "Server error! Please try again later. Err: ".$e;
+        }
+    }
+
     function displayExperience()
     {
         echo $this->experience_form_p1;
@@ -241,21 +332,10 @@ class Profile
     {
         echo $this->education_form_p1;
     }
-    private $tmp = "";
-    function displaySkills($getVarLang, $getVarSkill, $lang_table, $skills_table)
-    {
-        foreach ($lang_table as $key=>$value)
-        {
-            $this->tmp = $this->skills_form_mux1. $value .$this->skills_form_p2. $key .$this->skills_form_p3;
-        }
 
-        for ($i=0;$i <= $getVarLang;$i++)
-        {
-            for ($j=0; $j <= $getVarSkill; $j++)
-            { // TODO display multiple skills
-                $this->skills_form = $this->skills_form_p1. $this->tmp . $this->skills_form_mux2  .$this->skills_form_p4.$this->skills_form_p5;
-            }
-        }
+    function displaySkills()
+    {
+        $this->skills_form = $this->skills_form_p1. $this->tmp_l . $this->skills_form_mux2. $this->tmp_s . $this->skills_form_mux4;
         echo $this->skills_form;
     }
     function displayAdditional()
--- profile.php
+++ profile.php
@@ -5,75 +5,28 @@ session_start();
     header('Location: index.php');
     exit();
 }*/
-
-// set connection with db
 require_once "php/connect.php";
 require_once "php/Profile.php";
-
 $profileInstance = new Profile();
 
+// first form
 mysqli_report(MYSQLI_REPORT_STRICT);
 try
 {
-    $connection = new mysqli($host, $db_user, $db_pass, $db_name);
-    if ($connection->connect_errno != 0)
-    {
-        throw new Exception(mysqli_connect_errno());
-    } else
-    {
-        // table 1
-        $table_personal_data = $connection->query("SELECT u.name, u.surname, a.phone, c.locality As residence_city from users u join applicants a on u.id_user=a.id_user join cities c on a.id_city=c.id_city where u.id_user = '{$_SESSION['id_user']}'");
-        if (!$table_personal_data)
-        {
-            throw new Exception($connection->error);
-        }
-        $assoc_tpd = $table_personal_data->fetch_assoc();
-
-        // table 2
-        $table_experience = $connection->query("SELECT e.job, e.employer, e.start_job, e.end_job, c.locality As job_city, e.description As job_description from users u join applicants a on u.id_user=a.id_user join cities c on a.id_city=c.id_city join experiences e on a.id_applicants = e.id_applicants where u.id_user='{$_SESSION['id_user']}'");
-        if (!$table_experience)
-        {
-            throw new Exception($connection->error);
-        }
-        $count_tx = $table_experience->num_rows;
-        $assoc_tx = $table_experience->fetch_assoc();
-
-        // table 3
-        $table_education = $connection->query("SELECT s.name_school, s.specialization, s.start_learning, s.end_learning, c.locality As school_city, s.description As school_description from users u join applicants a on u.id_user=a.id_user join cities c on a.id_city=c.id_city join schools s on a.id_applicants=s.id_applicants where u.id_user='{$_SESSION['id_user']}'");
-        if (!$table_education)
-        {
-            throw new Exception($connection->error);
-        }
-        $count_te = $table_education->num_rows;
-        $assoc_te = $table_education->fetch_assoc();
-
-        // table 4.1
-        $table_lang = $connection->query("SELECT la.language, le.level FROM users u join applicants a on u.id_user=a.id_user join knowledge k on a.id_applicants=k.id_applicants join levels le on k.id_level=le.id_level JOIN languages la on k.id_language=la.id_language where u.id_user = '{$_SESSION['id_user']}'");
-        if (!$table_lang)
-        {
-            throw new Exception($connection->error);
-        }
-        $count_tl = $table_lang->num_rows;
-        $assoc_tl = $table_lang->fetch_assoc();
-
-        // table 4.2
-        $table_skills = $connection->query("SELECT s.sience, le.level FROM users u join applicants a on u.id_user=a.id_user join holders k on a.id_applicants=k.id_applicants join levels le on k.id_level=le.id_level join holders h on le.id_level=h.id_level join skills s on s.id_skill=h.id_skill where u.id_user = '{$_SESSION['id_user']}'");
-        if (!$table_skills)
-        {
-            throw new Exception($connection->error);
-        }
-        $count_ts = $table_skills->num_rows;
-        $assoc_ts = $table_skills->fetch_assoc();
-
-        // table 5
-        $table_additional = $connection->query("SELECT cv.description As cv_description, cl.description As cl_description, certifications.descriptions As cert_descriptions, t.training, t.description As course_description from users u join applicants a on u.id_user=a.id_user join cv on a.id_applicants=cv.id_applicants join certifications on a.id_applicants=certifications.id_applicants join training t on a.id_applicants=t.id_applicants join applications ap on a.id_applicants=ap.id_applicants join cl on ap.id_application=cl.id_application where u.id_user='{$_SESSION['id_user']}'");
-        if (!$table_additional)
-        {
-            throw new Exception($connection->error);
-        }
-        $count_ta = $table_additional->num_rows;
-        $assoc_ta = $table_additional->fetch_assoc();
-    }
+$connection = new mysqli($host, $db_user, $db_pass, $db_name);
+if ($connection->connect_errno != 0)
+{
+    throw new Exception(mysqli_connect_errno());
+} else
+{
+// table 1
+$table_personal_data = $connection->query("SELECT u.name, u.surname, a.phone, c.locality As residence_city from users u join applicants a on u.id_user=a.id_user join cities c on a.id_city=c.id_city where u.id_user = '{$_SESSION['id_user']}'");
+if (!$table_personal_data)
+{
+    throw new Exception($connection->error);
+}
+$assoc_tpd = $table_personal_data->fetch_assoc();
+}
 
     $connection->close();
 }
@@ -81,6 +34,8 @@ catch (Exception $e)
 {
     echo "Server error! Please try again later. Err: ".$e;
 }
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -168,9 +123,10 @@ catch (Exception $e)
             </div>
         </div>
         <?php
+        $profileInstance->fetchData($host, $db_user, $db_pass, $db_name);
         $profileInstance->displayExperience();
         $profileInstance->displayEducation();
-        $profileInstance->displaySkills($count_tl, $count_ts, $assoc_tl, $assoc_ts);
+        $profileInstance->displaySkills();
         $profileInstance->displayAdditional();
         // Porwałem twoje divy
         // i nie oddam
