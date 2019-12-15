c2c821d2dbbf37fa70041daf8eea8b84da1970da
Krzysztof Chodkiewicz <kchod.98@gmail.com>
2019-12-15 16:54:46 +0100

    working .json file

--- profile.php
+++ profile.php
@@ -8,75 +8,37 @@ session_start();
 require_once "php/connect.php";
 
 // array from DB
-$json_array = array(/*
-        "personal-data" => array(
-            "first-name" => "",
-            "last-name" => "",
-            "phone-number" => "",
-            "country" => "",
-            "city" => "",
-        ),
-        "experience" => array(
-            "no-experience" => false,
-            "job-title" => array(),
-            "employer" => array(),
-            "start-date" => array(),
-            "end-date" => array(),
-            "city" => array(),
-            "description" => array(),
-        ),
-        "education" => array(
-            "school" => array(),
-            "specialization" => array(),
-            "start-date" => array(),
-            "end-date" => array(),
-            "city" => array(),
-            "description" => array(),
-        ),
-        "skills" => array(
-            "languages" => array(
-                    "lang" => array(),
-                    "level" => array(),
-            ),
-            "skills" => array(
-                "skill" => array(),
-                "level" => array(),
-            ),
-        ),
-        "additional" => array(
-            "cv" => "",
-            "certificates" => array(),
-            "cover-letter" => array(),
-            "courses" => array(),
-        ),*/
-);
+$json_array = array();
 
 // tmp array
-$data_push_tpd = array();
-$data_push_tx = array();
-$data_push_te = array();
-$data_push_tl = array();
-$data_push_tll = array();
-$data_push_ts = array();
-$data_push_tsl = array();
+$data_push_tpdn = array(); $data_push_tpds = array(); $data_push_tpdp = array(); $data_push_tpdc = array(); $data_push_tpdl = array();
+$data_push_txj = array(); $data_push_txe = array(); $data_push_txsj = array(); $data_push_txej = array(); $data_push_txc = array(); $data_push_txd = array();
+$data_push_ten = array(); $data_push_tes = array(); $data_push_tesl = array(); $data_push_teel = array(); $data_push_tec = array(); $data_push_ted = array();
+$data_push_tl = array(); $data_push_tll = array();
+$data_push_ts = array(); $data_push_tsl = array();
 $data_push_tcv = array();
 $data_push_tcl = array();
 $data_push_tce = array();
 $data_push_tco = array();
 
-// queries TODO ja jebe jak tu duzo roboty
+// queries
 $query_tpdn = "SELECT u.name from users u join applicants a on u.id_user=a.id_user join cities c on a.id_city=c.id_city join countries co on a.id_country=co.id_country where u.id_user = '{$_SESSION['id_user']}'";
 $query_tpds = "SELECT u.surname from users u join applicants a on u.id_user=a.id_user join cities c on a.id_city=c.id_city join countries co on a.id_country=co.id_country where u.id_user = '{$_SESSION['id_user']}'";
 $query_tpdp = "SELECT a.phone from users u join applicants a on u.id_user=a.id_user join cities c on a.id_city=c.id_city join countries co on a.id_country=co.id_country where u.id_user = '{$_SESSION['id_user']}'";
 $query_tpdc = "SELECT co.country from users u join applicants a on u.id_user=a.id_user join cities c on a.id_city=c.id_city join countries co on a.id_country=co.id_country where u.id_user = '{$_SESSION['id_user']}'";
 $query_tpdl = "SELECT c.locality As residence_city from users u join applicants a on u.id_user=a.id_user join cities c on a.id_city=c.id_city join countries co on a.id_country=co.id_country where u.id_user = '{$_SESSION['id_user']}'";
-$query_tx = "SELECT e.job, e.employer, e.start_job, e.end_job, c.locality As job_city, e.description As job_description from users u join applicants a on u.id_user=a.id_user join experiences e on a.id_applicants = e.id_applicants join cities c on e.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
-$query_tx = "SELECT e.job, e.employer, e.start_job, e.end_job, c.locality As job_city, e.description As job_description from users u join applicants a on u.id_user=a.id_user join experiences e on a.id_applicants = e.id_applicants join cities c on e.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
-$query_tx = "SELECT e.job, e.employer, e.start_job, e.end_job, c.locality As job_city, e.description As job_description from users u join applicants a on u.id_user=a.id_user join experiences e on a.id_applicants = e.id_applicants join cities c on e.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
-$query_tx = "SELECT e.job, e.employer, e.start_job, e.end_job, c.locality As job_city, e.description As job_description from users u join applicants a on u.id_user=a.id_user join experiences e on a.id_applicants = e.id_applicants join cities c on e.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
-$query_tx = "SELECT e.job, e.employer, e.start_job, e.end_job, c.locality As job_city, e.description As job_description from users u join applicants a on u.id_user=a.id_user join experiences e on a.id_applicants = e.id_applicants join cities c on e.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
-$query_tx = "SELECT e.job, e.employer, e.start_job, e.end_job, c.locality As job_city, e.description As job_description from users u join applicants a on u.id_user=a.id_user join experiences e on a.id_applicants = e.id_applicants join cities c on e.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
-$query_te = "SELECT s.name_school, s.specialization, s.start_learning, s.end_learning, c.locality As school_city, s.description As school_description from users u join applicants a on u.id_user=a.id_user join schools s on a.id_applicants=s.id_applicants join cities c on s.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
+$query_txj = "SELECT e.job from users u join applicants a on u.id_user=a.id_user join experiences e on a.id_applicants = e.id_applicants join cities c on e.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
+$query_txe = "SELECT e.employer from users u join applicants a on u.id_user=a.id_user join experiences e on a.id_applicants = e.id_applicants join cities c on e.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
+$query_txsj = "SELECT e.start_job from users u join applicants a on u.id_user=a.id_user join experiences e on a.id_applicants = e.id_applicants join cities c on e.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
+$query_txej = "SELECT e.end_job from users u join applicants a on u.id_user=a.id_user join experiences e on a.id_applicants = e.id_applicants join cities c on e.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
+$query_txc = "SELECT c.locality As job_city from users u join applicants a on u.id_user=a.id_user join experiences e on a.id_applicants = e.id_applicants join cities c on e.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
+$query_txd = "SELECT e.description As job_description from users u join applicants a on u.id_user=a.id_user join experiences e on a.id_applicants = e.id_applicants join cities c on e.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
+$query_ten = "SELECT s.name_school from users u join applicants a on u.id_user=a.id_user join schools s on a.id_applicants=s.id_applicants join cities c on s.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
+$query_tes = "SELECT s.specialization from users u join applicants a on u.id_user=a.id_user join schools s on a.id_applicants=s.id_applicants join cities c on s.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
+$query_tesl = "SELECT s.start_learning from users u join applicants a on u.id_user=a.id_user join schools s on a.id_applicants=s.id_applicants join cities c on s.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
+$query_teel = "SELECT s.end_learning from users u join applicants a on u.id_user=a.id_user join schools s on a.id_applicants=s.id_applicants join cities c on s.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
+$query_tec = "SELECT c.locality As school_city from users u join applicants a on u.id_user=a.id_user join schools s on a.id_applicants=s.id_applicants join cities c on s.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
+$query_ted = "SELECT s.description As school_description from users u join applicants a on u.id_user=a.id_user join schools s on a.id_applicants=s.id_applicants join cities c on s.id_city=c.id_city where u.id_user='{$_SESSION['id_user']}'";
 $query_tl = "SELECT la.language from users u join applicants a on u.id_user=a.id_user join knowledge k on a.id_applicants=k.id_applicants join languages la on k.id_language=la.id_language where u.id_user = '{$_SESSION['id_user']}'";
 $query_tll = "SELECT le.id_level from users u join applicants a on u.id_user=a.id_user join knowledge k on a.id_applicants=k.id_applicants join levels le on k.id_level=le.id_level join languages la on k.id_language=la.id_language where u.id_user = '{$_SESSION['id_user']}'";
 $query_ts = "SELECT s.sience from users u join applicants a on u.id_user=a.id_user join holders k on a.id_applicants=k.id_applicants join skills s on s.id_skill=k.id_skill where u.id_user = '{$_SESSION['id_user']}'";
@@ -88,10 +50,9 @@ $query_tco = "SELECT t.description As course_description from users u join appli
 
 // fetch data and add it to .json file
 function fetchData($query, &$data_push, &$array, $host, $db_user, $db_pass, $db_name) {
-
+    $counter = 0;
     $connection = new mysqli($host, $db_user, $db_pass, $db_name);
     if ($connection->connect_errno != 0) {
-        $connection->close();
         throw new Exception(mysqli_connect_errno());
     }
     else {
@@ -99,16 +60,16 @@ function fetchData($query, &$data_push, &$array, $host, $db_user, $db_pass, $db_
         if (!$table) {
             throw new Exception($connection->error);
         }
+        $counter = $table->num_rows;
         while ($assoc = $table->fetch_assoc()) {
             foreach ($assoc as $key => $value) {
                 @array_push($data_push, $value);
                 $array = $data_push;
             }
         }
-        $connection->close();
-        return $table->num_rows;
     }
-
+    $connection->close();
+    return $counter;
 }
 
 
@@ -116,9 +77,23 @@ function fetchData($query, &$data_push, &$array, $host, $db_user, $db_pass, $db_
 mysqli_report(MYSQLI_REPORT_STRICT);
 try
 {
-    $count_tpd = fetchData($query_tpdn, $data_push_tpd, $json_array['personal-data'], $host, $db_user, $db_pass, $db_name);
-    $count_tx = fetchData($query_tx, $data_push_tx, $json_array['experience'], $host, $db_user, $db_pass, $db_name);
-    $count_te = fetchData($query_te, $data_push_te, $json_array['education'], $host, $db_user, $db_pass, $db_name);
+    $count_tpd = fetchData($query_tpdn, $data_push_tpdn, $json_array['personal-data']['first-name'], $host, $db_user, $db_pass, $db_name);
+    $count_tpd = fetchData($query_tpds, $data_push_tpds, $json_array['personal-data']['last-name'], $host, $db_user, $db_pass, $db_name);
+    $count_tpd = fetchData($query_tpdp, $data_push_tpdp, $json_array['personal-data']['phone'], $host, $db_user, $db_pass, $db_name);
+    $count_tpd = fetchData($query_tpdc, $data_push_tpdc, $json_array['personal-data']['country'], $host, $db_user, $db_pass, $db_name);
+    $count_tpd = fetchData($query_tpdl, $data_push_tpdl, $json_array['personal-data']['city'], $host, $db_user, $db_pass, $db_name);
+    $count_tx = fetchData($query_txj, $data_push_txj, $json_array['experience']['job-title'], $host, $db_user, $db_pass, $db_name);
+    $count_tx = fetchData($query_txe, $data_push_txe, $json_array['experience']['employer'], $host, $db_user, $db_pass, $db_name);
+    $count_tx = fetchData($query_txsj, $data_push_txsj, $json_array['experience']['start-date'], $host, $db_user, $db_pass, $db_name);
+    $count_tx = fetchData($query_txej, $data_push_txej, $json_array['experience']['end-date'], $host, $db_user, $db_pass, $db_name);
+    $count_tx = fetchData($query_txc, $data_push_txc, $json_array['experience']['city'], $host, $db_user, $db_pass, $db_name);
+    $count_tx = fetchData($query_txd, $data_push_txd, $json_array['experience']['description'], $host, $db_user, $db_pass, $db_name);
+    $count_te = fetchData($query_ten, $data_push_ten, $json_array['education']['school-name'], $host, $db_user, $db_pass, $db_name);
+    $count_te = fetchData($query_tes, $data_push_tes, $json_array['education']['specialization'], $host, $db_user, $db_pass, $db_name);
+    $count_te = fetchData($query_tesl, $data_push_tesl, $json_array['education']['start-date'], $host, $db_user, $db_pass, $db_name);
+    $count_te = fetchData($query_teel, $data_push_teel, $json_array['education']['end-date'], $host, $db_user, $db_pass, $db_name);
+    $count_te = fetchData($query_tec, $data_push_tec, $json_array['education']['city'], $host, $db_user, $db_pass, $db_name);
+    $count_te = fetchData($query_ted, $data_push_ted, $json_array['education']['description'], $host, $db_user, $db_pass, $db_name);
     $count_tl = fetchData($query_tl, $data_push_tl, $json_array['skills']['languages']['lang'], $host, $db_user, $db_pass, $db_name);
     $count_tll = fetchData($query_tll, $data_push_tll, $json_array['skills']['languages']['level'], $host, $db_user, $db_pass, $db_name);
     $count_ts = fetchData($query_ts, $data_push_ts, $json_array['skills']['skills']['skill'], $host, $db_user, $db_pass, $db_name);
@@ -127,177 +102,7 @@ try
     $count_tcl = fetchData($query_tcl, $data_push_tcl, $json_array['additional']['cover-letter'], $host, $db_user, $db_pass, $db_name);
     $count_tce = fetchData($query_tce, $data_push_tce, $json_array['additional']['certificates'], $host, $db_user, $db_pass, $db_name);
     $count_tco = fetchData($query_tco, $data_push_tco, $json_array['additional']['courses'], $host, $db_user, $db_pass, $db_name);
-    /*$connection = new mysqli($host, $db_user, $db_pass, $db_name);
-    if ($connection->connect_errno != 0)
-    {
-        throw new Exception(mysqli_connect_errno());
-    }
-    else
-    {
-        //echo $count_tx;
-        // table 1
-        $table_personal_data = $connection->query($query_tpd);
-        if (!$table_personal_data)
-            {
-                throw new Exception($connection->error);
-            }
-        while ($assoc_tpd = $table_personal_data->fetch_assoc())
-        {
-            foreach ($assoc_tpd as $key=>$value)
-            {
-                array_push($data_push_tpd, $value);
-                $json_array['personal-data'] = $data_push_tpd;
-            }
-        }
-        // table 2
-        $table_experience = $connection->query($query_tx);
-        if (!$table_experience)
-        {
-            throw new Exception($connection->error);
-        }
-        $count_tx = $table_experience->num_rows;
-        while ($assoc_tx = $table_experience->fetch_assoc())
-        {
-            foreach ($assoc_tx as $key=>$value)
-            {
-                array_push($data_push_tx, $value);
-                $json_array['experience'] = $data_push_tx;
-            }
-        }
 
-        // table 3
-        $table_education = $connection->query($query_te);
-        if (!$table_education)
-        {
-            throw new Exception($connection->error);
-        }
-        $count_te = $table_education->num_rows;
-        while ($assoc_te = $table_education->fetch_assoc())
-        {
-            foreach ($assoc_te as $key=>$value)
-            {
-                array_push($data_push_te, $value);
-                $json_array['education'] = $data_push_te;
-            }
-        }
-
-        // table 4.1
-        $table_lang = $connection->query($query_tl);
-        $table_lang_level = $connection->query($query_tll);
-        if (!$table_lang || !$table_lang_level)
-        {
-            throw new Exception($connection->error);
-        }
-        $count_tl = $table_lang->num_rows;
-        while ($assoc_tl = $table_lang->fetch_assoc())
-        {
-                foreach ($assoc_tl as $key=>$value)
-                {
-                    array_push($data_push_tl, $value);
-                    $json_array['skills']['languages']['lang'] = $data_push_tl;
-                }
-        }
-        while ((int)$assoc_tll = $table_lang_level->fetch_assoc())
-        {
-            foreach ($assoc_tll as $keyL=>$valueL)
-            {
-                foreach ($assoc_tll as $key=>$value)
-                {
-                    array_push($data_push_tll, $value);
-                    $json_array['skills']['languages']['level'] = $data_push_tll;
-                }
-            }
-        }
-
-        // table 4.2
-        $table_skills = $connection->query($query_ts);
-        $table_skills_level = $connection->query($query_tsl);
-        if (!$table_skills || !$table_skills_level)
-        {
-            throw new Exception($connection->error);
-        }
-        $count_ts = $table_skills->num_rows;
-
-        while ($assoc_ts = $table_skills->fetch_assoc())
-        {
-               foreach ($assoc_ts as $key=>$value)
-                {
-                    array_push($data_push_ts, $value);
-                    $json_array['skills']['skills']['skill'] = $data_push_ts;
-                }
-        }
-        while ($assoc_tsl = $table_skills_level->fetch_assoc()) // TODO incorrect query
-        {
-            foreach ($assoc_tsl as $key=>$value)
-            {
-                array_push($data_push_tsl, $value);
-                $json_array['skills']['skills']['level'] = $data_push_tsl;
-            }
-        }
-
-        // table 5.1
-        $table_cv = $connection->query($query_tcv);
-        if (!$table_cv)
-        {
-            throw new Exception($connection->error);
-        }
-        $count_tcv = $table_cv->num_rows;
-        while ($assoc_tcv = $table_cv->fetch_assoc())
-        {
-            foreach ($assoc_tcv as $key=>$value)
-            {
-                array_push($data_push_tcv, $value);
-                $json_array['additional']['cv'] = $data_push_tcv;
-            }
-        }
-        // table 5.2
-        $table_cl = $connection->query($query_tcl);
-        if (!$table_cl)
-        {
-            throw new Exception($connection->error);
-        }
-        $count_tcl = $table_cl->num_rows;
-        while ($assoc_tcl = $table_cl->fetch_assoc())
-        {
-            foreach ($assoc_tcl as $key=>$value)
-            {
-                array_push($data_push_tcl, $value);
-                $json_array['additional']['cover-letter'] = $data_push_tcl;
-            }
-        }
-        // table 5.3
-        $table_ce = $connection->query($query_tce);
-        if (!$table_ce)
-        {
-            throw new Exception($connection->error);
-        }
-        $count_tce = $table_ce->num_rows;
-        while ($assoc_tce = $table_ce->fetch_assoc())
-        {
-            foreach ($assoc_tce as $key=>$value)
-            {
-                array_push($data_push_tce, $value);
-                $json_array['additional']['certificates'] = $data_push_tce;
-            }
-        }
-        // table 5.4
-        $table_co = $connection->query($query_tco);
-        if (!$table_co)
-        {
-            throw new Exception($connection->error);
-        }
-        $count_tco = $table_co->num_rows;
-        while ($assoc_tco = $table_co->fetch_assoc())
-        {
-            foreach ($assoc_tco as $key=>$value)
-            {
-                array_push($data_push_tco, $value);
-                $json_array['additional']['courses'] = $data_push_tco;
-            }
-        }
-
-
-    }*/
     //fill .json file with data from db
     $fp = fopen('profile_data.json', 'w');
     fwrite($fp, json_encode($json_array));
@@ -308,7 +113,6 @@ catch (Exception $e)
 {
     echo "<div class='server-error'>Server error! Please try again later. Err: ".$e."</div>";
 }
-
 
 ?>
 <!DOCTYPE html>
