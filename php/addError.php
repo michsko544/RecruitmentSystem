704c76f5b7d11330b574f9ccb04320dc9a0416cb
kchodkiewicz <you@example.com>
2020-02-07 17:11:53 +0100

    fix log

--- php/addError.php
+++ php/addError.php
@@ -2,7 +2,7 @@
 function addError($e){
     $a = array();
     $a['serverError'] = strval($e);
-    $fp = fopen($_SERVER['DOCUMENT_ROOT'] . '/json/log.json', 'c');
+    $fp = fopen($_SERVER['DOCUMENT_ROOT'] . '/json/log.json', 'w');
     fwrite($fp, json_encode($a));
     fclose($fp);
 }
--- php/connect.php
+++ php/connect.php
@@ -1,6 +1,6 @@
 <?php
 
 $host = 'localhost'; // host address
-$db_user = "rootw"; // username
+$db_user = "root"; // username
 $db_pass = ""; // password
 $db_name = "recruitment"; // database name
