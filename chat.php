3d9287be32d8ddbab58bc8b5bb5daba5ec1f60dc
Krzysztof Chodkiewicz <kchod.98@gmail.com>
2020-02-09 22:03:50 +0100

    .

--- chat.php
+++ chat.php
@@ -8,6 +8,9 @@ require_once "php/connect.php";
 require_once "php/getRole.php";
 getRole($host, $db_user, $db_pass, $db_name);
 require_once "php/chat.php";
+if ((isset($_GET['aid'])) && (isset($_GET['uid']))){
+    getUserName($_GET['aid'], $_GET['uid']);
+}
 if (isset($_GET['cid'])){
     $id_conv = $_GET['cid'];
     $_SESSION['id_conv'] = $id_conv;
